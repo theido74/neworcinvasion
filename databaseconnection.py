fb6b7901374ae26121d3d52e8fd817aef92d7edc
theido74 <147144073+theido74@users.noreply.github.com>
2023-11-21 11:39:12 +0100

    Update databaseconnection.py

--- databaseconnection.py
+++ databaseconnection.py
@@ -6,7 +6,7 @@ class DBConnection():
     userid = ''
     def __init__(self):
         try:
-            self.connection = mysql.connector.connect(user='root', password='LaCasita2022',
+            self.connection = mysql.connector.connect(user='root', password='****',
                                           host='127.0.0.1',
                                           database='datatest')
             self.lastmessage = "Connection successful"
