eb5f012d39551d0f96b66aefd596d5459b03e115
swannty <swannty@hotmail.com>
2023-11-21 14:09:44 +0100

    switch path files

---
+++ path_dos2nix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed -i 's/\\/\//g' bonus.py
+sed -i 's/\\/\//g' enemy.py
+sed -i 's/\\/\//g' explose.py
+sed -i 's/\\/\//g' levellogic.py
+sed -i 's/\\/\//g' login.py
+sed -i 's/\\/\//g' player.py
+sed -i 's/\\/\//g' projectiles.py
+sed -i 's/\\/\//g' sound.py
+sed -i 's/\\/\//g' text.py
+
